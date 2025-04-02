b86139c2713fcd7546fdc11f7c7fbaa03b66859e
GODAOS <jmskkr@gmail.com>
2025-04-02 09:43:17 +0900

    first init

---
+++ src/main/kotlin/com/example/docker_pratice/DockerPraticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.docker_pratice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DockerPraticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<DockerPraticeApplication>(*args)
+}
